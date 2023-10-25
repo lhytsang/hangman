ee2ba7dee58a276f5662d16f61e555b0de6bdda1
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-10-25 20:45:33 +0100

    Checks if the input is alphabetical and also only a single letter.

--- milestone_2.py
+++ milestone_2.py
@@ -9,3 +9,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Please enter a single letter: ")
+
+if guess.isalpha() and len(guess) == 1:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input." )
