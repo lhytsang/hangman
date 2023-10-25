9bc61dabd153991b5be5cdc0ce9657cec1f6aeb3
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-10-25 23:27:34 +0100

    Removing first instance of "Good guess" so that user is only complimented if their guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -8,7 +8,6 @@ check_letter = True
 while check_letter == True:
     guess = input("Please enter a single letter: ")
     if guess.isalpha() and len(guess) == 1:
-        print("Good guess!")
         check_letter = False
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
