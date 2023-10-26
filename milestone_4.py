d2b61c38c6d013966a7dbbd1139b613a46c6e36c
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-10-26 22:28:07 +0100

    Create new attribure 'word()' which randomly chooses a word from 'word_list' using the .choice() function in the random mmodule.

--- milestone_4.py
+++ milestone_4.py
@@ -5,3 +5,7 @@ class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
+
+    def word(word_list):
+        word = random.choice(word_list)
+        
