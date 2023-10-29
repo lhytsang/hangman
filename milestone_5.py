6b5c60cd07c7b34b59c6c64e9489cc01fed434a0
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-10-29 01:55:31 +0100

    Copying the code from 'milestone_4.py' into 'milestone_5.py'.

---
+++ milestone_5.py
@@ -0,0 +1,42 @@
+import random
+
+class Hangman:
+
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ['-'] * len(self.word)
+        self.num_letters = len(set(self.word))
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+
+        if guess in self.word:
+            self.guess_list.append(guess)
+            print(f"Good guess! {guess} is in the word")
+
+            for letter in self.word:
+                indices = [self.word.index(letter, position, len(self.word)) for position in range(len(self.word)) if letter == guess]
+                self.word_guessed = [self.word_guessed[num] == guess for num in indices]
+            
+            self.num_letters -= 1
+
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word. \n You have {self.num_lives} lives left.")
+   
+    def ask_for_input(self):
+        check_letter = True
+
+        while check_letter == True:
+            guess = input("Please enter a single letter: ")
+            
+            if not(guess.isalpha() and len(guess) == 1):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                Hangman.check_guess(guess)
+                self.list_of_guesses.append(guess)
